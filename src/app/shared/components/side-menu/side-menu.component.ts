940f62b4c8088e916e7cb7c8ab992e2a5673f301
David Cruz <davidcruz127@gmail.com>
2023-10-18 21:08:59 +0100

    Sección 17

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  title: string,
+  route: string
+}
+
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.css']
+})
+export class SideMenuComponent {
+
+  public reactiveMenu: MenuItem[] = [
+    { title: 'Básicos', route: './reactive/basic'},
+    { title: 'Dinamicos', route: './reactive/dynamic'},
+    { title: 'Switches', route: './reactive/switches'}
+  ];
+
+
+  public authMenu: MenuItem[] = [
+    { title: 'Registro', route: './auth'}
+  ];
+
+}
